f340484a24c2d828636123a0d345d82d9c43a0ca
Ben Chen <idben.chen@gmail.com>
2026-07-14 09:21:06 +0800

    try 的基本格式

---
+++ try.py
@@ -0,0 +1,12 @@
+# 錯誤與例外的處理
+try:
+    num = int(input("請輸入一組數字"))
+    result = 10 / num
+except ValueError as e:
+    result = f"輸入了無效的數字，{e}"
+except ZeroDivisionError as e:
+    result = f"被除數不能是零，{e}"
+except:
+    result = "有錯誤發生"
+
+print(f"結果是: {result}")
